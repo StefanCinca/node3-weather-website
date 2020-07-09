7570eccc05b772a6c4c89fd1056cd9f02a383dab
Stefan Cinca <stefancinca@Daniels-MacBook-Pro.local>
2020-07-09 15:49:02 +0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener("submit", (e) =>{
     console.log("testing");
     const address = searchElement.value;
     console.log(address);
-    fetch("http://localhost:3000/weather?address=" + address).then((response) =>{
+    fetch("/weather?address=" + address).then((response) =>{
         response.json().then((data) => {
             if (data.error){
                 console.log(data.error);
